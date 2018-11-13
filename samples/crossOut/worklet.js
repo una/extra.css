7bbbaab2a9952ff9c2b633420cab58ee5159f8f5
Una Kravets <una.kravets@gmail.com>
2018-11-13 22:16:32 +0000

    working on the kinks with centering as it resizes

--- samples/crossOut/worklet.js
+++ samples/crossOut/worklet.js
@@ -12,17 +12,17 @@ if (typeof registerPaint !== 'undefined') {
       const lineWidth = properties.get('--extra-crossWidth')
       const color = properties.get('--extra-crossColor')
       const spread = properties.get('--extra-crossSpread')
-      const spreadPercent = spread / 100
-      const verticalStart = (size.height - (size.height * spreadPercent)) * 1.5
+      const spreadPercent = (spread - 1) / 100
+      const verticalStart = (size.height - lineWidth) * spreadPercent + lineWidth
 
       ctx.lineWidth = lineWidth
       ctx.strokeStyle = color
 
       ctx.beginPath()
-      ctx.moveTo(lineWidth, verticalStart + lineWidth)
-      ctx.lineTo(size.width - lineWidth, size.height * spreadPercent - lineWidth)
-      ctx.moveTo(size.width - lineWidth, verticalStart + lineWidth)
-      ctx.lineTo(lineWidth, size.height * spreadPercent - lineWidth)
+      ctx.moveTo(lineWidth, lineWidth)
+      ctx.lineTo(size.width - lineWidth, verticalStart)
+      ctx.moveTo(size.width - lineWidth, lineWidth)
+      ctx.lineTo(lineWidth, verticalStart + size.height)
       ctx.stroke()
     }
   }
