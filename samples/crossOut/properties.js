c478c8d0c471ccde79885e6762dacba822273c36
Una Kravets <una.kravets@gmail.com>
2018-11-13 16:21:42 +0100

    adding crossOut, working on centering with spread

---
+++ samples/crossOut/properties.js
@@ -0,0 +1,20 @@
+CSS.registerProperty({
+  name: '--extra-crossColor',
+  syntax: '<color>',
+  inherits: true,
+  initialValue: 'black'
+});
+
+CSS.registerProperty({
+  name: '--extra-crossSpread',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 100
+});
+
+CSS.registerProperty({
+  name: '--extra-crossWidth',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 1
+});
---
+++ samples/crossOut/worklet.js
@@ -0,0 +1,31 @@
+const getRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+if (typeof registerPaint !== 'undefined') {
+  class CrossOut {
+    static get inputProperties() {
+      return ['--extra-crossNumber', '--extra-crossColor', '--extra-crossSpread', '--extra-crossWidth']
+    }
+  
+    paint(ctx, size, properties) {
+      const lineWidth = properties.get('--extra-crossWidth')
+      const color = properties.get('--extra-crossColor')
+      const spread = properties.get('--extra-crossSpread')
+      const spreadPercent = spread / 100
+      const verticalStart = (size.height - (size.height * spreadPercent)) / 2
+
+      ctx.lineWidth = lineWidth
+      ctx.strokeStyle = color
+
+      ctx.beginPath()
+      ctx.moveTo(lineWidth, verticalStart + lineWidth)
+      ctx.lineTo(size.width - lineWidth, size.height * spreadPercent - lineWidth)
+      ctx.moveTo(size.width - lineWidth, verticalStart + lineWidth)
+      ctx.lineTo(lineWidth, size.height * spreadPercent - lineWidth)
+      ctx.stroke()
+    }
+  }
+  
+  registerPaint('extra-crossOut', CrossOut)
+}
