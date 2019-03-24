af969dfb3cd34b7eb21d2d27a94a6e805e7df618
Una Kravets <una.kravets@gmail.com>
2019-03-24 18:02:31 -0400

    move layout into component

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import CrossOut from '../components/Demos/CrossOut'
@@ -7,22 +7,34 @@ import SuperUnderline from '../components/Demos/SuperUnderline'
 import Sparkles from '../components/Demos/Sparkles'
 import style from './style.module.css'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`houdini`, `css`, `library`, `extra.css`]} />
-    {!('paintWorklet' in CSS) && (<p className={style.warning}>WARNING: Your browser does not currently support <a href="https://caniuse.com/#search=houdini">Houdini</a>, and you won't be able to see any demos 😞</p>)
-    }
-    <p className={style.intro}>A CSS <a href="http://ishoudinireadyyet.com">Houdini</a> library for making your site a little more <span>#extra</span>.</p>
-    
-    <div className={style.demos}>
-      {/* All the demos */}
-      <Sparkles />
-      <ScallopedBorder />
-      <SuperUnderline />
-      <CrossOut />
-    </div>
-    
-  </Layout>
-)
+export default class IndexPage extends Component {
+  state = {
+    noSupport: false
+  }
 
-export default IndexPage
+  componentDidMount () {
+    if (typeof window !== 'undefined' && !('paintWorklet' in window.CSS)) {
+      this.setState({ noSupport: true })
+    }
+  }
+  
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`houdini`, `css`, `library`, `extra.css`]} />
+        {this.state.noSupport && (<p className={style.warning}>WARNING: Your browser does not currently support <a href="https://caniuse.com/#search=houdini">Houdini</a>, and you won't be able to see any demos 😢</p>)
+        }
+        <p className={style.intro}>A CSS <a href="http://ishoudinireadyyet.com">Houdini</a> library for making your site a little more <span>#extra</span>.</p>
+        
+        <div className={style.demos}>
+          {/* All the demos */}
+          <Sparkles />
+          <ScallopedBorder />
+          <SuperUnderline />
+          <CrossOut />
+        </div>
+        
+      </Layout>
+    )
+  }
+}
